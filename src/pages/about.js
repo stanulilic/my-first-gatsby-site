a7b686bebcc2f7d0fbb1960d876a5c4b88fc38f4
Stanley Ulili <stanleyulili@gmail.com>
2021-08-06 10:45:59 +0200

    Finished Gatsby Tutorial Part 2

---
+++ src/pages/about.js
@@ -0,0 +1,15 @@
+import * as React from "react";
+import Layout from "../components/layout";
+
+const AboutPage = () => {
+  return (
+    <Layout pageTitle="About Me">
+      <p>
+        Hi there, this is my about me page. It contains information about me and
+        I plan on adding more content soon to this page. Thank you
+      </p>
+    </Layout>
+  );
+};
+
+export default AboutPage;
