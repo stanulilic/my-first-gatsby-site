dd9a977876b546d1ccf300071d2d47fc9b4b0e84
Stanley Ulili <stanleyulili@gmail.com>
2021-08-07 13:52:12 +0200

    commit left files

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,18 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: `https://www.yourdomain.tld`,
+    title: "Gatsby site",
+    siteUrl: `https://www.stanleyulili.com`,
   },
   plugins: [
     "gatsby-plugin-gatsby-cloud",
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      },
+    },
   ],
 };
